cac8b7a05633cabf05725d3e99b5e434ae49b336
shafriki <shafriki.cse@gmail.com>
2024-12-04 03:00:28 +0600

    extra section Guide added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../components/Banner';
 import Funding from '../components/Funding';
+import Guide from '../components/Guide';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Funding></Funding>
+            <Guide></Guide>
         </div>
     );
 };
