491d1acb21971a49dd86c4d774f54a1c233dc85c
shafriki <shafriki.cse@gmail.com>
2024-12-04 00:57:45 +0600

    added one extra section Funding

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../components/Banner';
+import Funding from '../components/Funding';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Funding></Funding>
         </div>
     );
 };
